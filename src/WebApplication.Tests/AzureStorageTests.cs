359ac3cacac58dc8c3ab508e47029ac8ba84dec2
Drewan <drewan.obrien@cashconverts.com>
2016-11-07 21:37:11 +0800

    move webrole back to 4.5.1

---
+++ src/WebApplication.Tests/AzureStorageTests.cs
@@ -0,0 +1,26 @@
+﻿using System.Linq;
+using Lokad.Cloud.Storage;
+using Xunit;
+
+namespace WebApplication.Tests
+{
+    public class AzureStorageTests : InMemoryStorageFixture
+    {
+        [Fact]
+        public void CanWriteBlobToMemory()
+        {
+            var metadata = new Message
+            {
+                SequenceId = 2,
+                RowKey = "126D1938-034C-449E-9E3B-1395BB106816"
+            };
+            var message = new Event { Content = "{}" };
+
+            Storage.PutBlob(metadata, message);
+
+            var blob = Storage.ListBlobs<Event>("events").Single();
+
+            Assert.Equal(blob.Content, "{}");
+        }
+    }
+}
---
+++ src/WebApplication.Tests/InMemoryStorageFixture.cs
@@ -0,0 +1,19 @@
+﻿using Lokad.Cloud.Storage;
+
+namespace WebApplication.Tests
+{
+    public class InMemoryStorageFixture
+    {
+        protected const string ContainerName = "events";
+
+        public InMemoryStorageFixture()
+        {
+            Storage = CloudStorage
+                .ForInMemoryStorage()
+                .BuildBlobStorage();
+            Storage.CreateContainerIfNotExist(ContainerName);
+        }
+
+        public IBlobStorageProvider Storage { get; set; }
+    }
+}
